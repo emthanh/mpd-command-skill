0a9b5fee8cde46f7c38d58444746991d54a48c62
emthanh <emthanh@free.fr>
2021-09-13 23:36:28 +0200

    initial import

--- __init__.py
+++ __init__.py
@@ -1,13 +1,31 @@
-from mycroft import MycroftSkill, intent_file_handler
+import subprocess
+from mycroft import MycroftSkill, intent_handler
 
 
 class MpdCommand(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('command.mpd.intent')
-    def handle_command_mpd(self, message):
-        self.speak_dialog('command.mpd')
+    @intent_handler('play.mpd.intent')
+    def handle_play_mpd(self, message):
+        self.speak_dialog('play.mpd')
+        text = subprocess.Popen(('mpc', 'play'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        output, errors = text.communicate()
+
+    @intent_handler('stop.mpd.intent')
+    def handle_play_mpd(self, message):
+        self.speak_dialog('stop.mpd')
+        text = subprocess.Popen(('mpc', 'stop'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        output, errors = text.communicate()
+
+    @intent_handler('info.mpd.intent')
+    def handle_info_mpd(self, message):
+        self.speak_dialog('info.mpd')
+
+        text = subprocess.Popen(('mpc', 'current'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        output, errors = text.communicate()
+        self.speak(output)
+
 
 
 def create_skill():
